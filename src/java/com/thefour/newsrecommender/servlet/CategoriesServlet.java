2e1858849e31669df5e3430106efaa84c03db767
quangsang <whitefang.tcotw@gmail.com>
2015-10-08 11:06:50 +0700

    change categories servlet pattern
    change categories servlet pattern "/categoriesservlet" -> "/categories"

--- src/java/com/thefour/newsrecommender/servlet/CategoriesServlet.java
+++ src/java/com/thefour/newsrecommender/servlet/CategoriesServlet.java
@@ -8,14 +8,18 @@ package com.thefour.newsrecommender.servlet;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /**
  *
  * @author Quang Quang
  */
+@WebServlet(name = "CategoriesServlet", urlPatterns = {"/categories"})
+
 public class CategoriesServlet extends HttpServlet {
 
     /**
